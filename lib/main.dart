603a96b8a73aa33427f391f5df7b3bafb071992f
sunteadrink <sunteadrink@hotmail.com>
2020-09-06 23:55:03 +0800

    i changed ur mom and dad ur sis

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times: ur mother ur father',
+              'You have pushed the button this many times: ur mother ur father ur sister',
             ),
             Text(
               '$_counter',
