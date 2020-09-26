1c4440f00a685583e8647235f27bd328f354fa30
liyanang <liyananggg@gmail.com>
2020-09-26 15:34:40 +0800

    Profile page UI

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:lifebalance/screens/services/auth.dart';
 //import 'package:lifebalance/screens/services/database.dart';
 //import 'package:provider/provider.dart';
+//import 'package:lifebalance/screens/CalendarPage.dart';
+import 'package:lifebalance/theme/colors/light_colors.dart';
+//import 'package:percent_indicator/percent_indicator.dart';
+import 'package:lifebalance/widgets/active_project_card.dart';
+import 'package:lifebalance/widgets/top_container.dart';
 
 class profile_page extends StatefulWidget {
 
@@ -16,6 +21,29 @@ class _profile_pageState extends State<profile_page> {
 
   @override
   Widget build(BuildContext context) {
+    Text subheading(String title) {
+      return Text(
+        title,
+        style: TextStyle(
+            color: LightColors.kDarkBlue,
+            fontSize: 20.0,
+            fontWeight: FontWeight.w700,
+            letterSpacing: 1.2),
+      );
+    }
+    double width = MediaQuery.of(context).size.width;
+
+    CircleAvatar calendarIcon() {
+      return CircleAvatar(
+        radius: 25.0,
+        backgroundColor: LightColors.kGreen,
+        child: Icon(
+          Icons.calendar_today,
+          size: 20.0,
+          color: Colors.white,
+        ),
+      );
+    }
     /*return StreamProvider<List<Course>>.value(
           value: DatabaseService().courses,
           child: Scaffold(*/
@@ -23,55 +51,160 @@ class _profile_pageState extends State<profile_page> {
         child: Scaffold(
             backgroundColor: Colors.white,
             appBar: AppBar(
-               title: Text('Life Balance'),
-              backgroundColor: Colors.blue[400],
+              title: Text('Life Balance'),
+              backgroundColor: LightColors.kGreen,
               elevation: 0.0,
               actions: <Widget>[
                 FlatButton.icon(
-                  icon: Icon(Icons.person),
-                  label: Text('Logout'),
+                  icon: Icon(Icons.person, color: Colors.white,),
+                  label: Text('Logout', style: TextStyle(color: Colors.white),),
                   onPressed: () async {
                     await _auth.signOut(); //return null value to user
                   },
                 ),
               ],
             ),
-
-            /* body: Container(
-          padding: const EdgeInsets.all(32),
-           child: Row(children: [
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Image.asset(
-                  'images/avatar.png',
-                  width: 100.0,
-                  height: 100.0,
-                  fit: BoxFit.cover,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                TopContainer(
+                  height: 200,
+                  width: width,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'edit',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ]
+                    ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 0, vertical: 0.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                          CircleAvatar(
+                            radius: 50.0,
+                            backgroundImage: AssetImage(
+                              'assets/images/avatar.png',
+                            ),
+                          ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Container(
+                                    child: Text(
+                                      'DIP Group 6',
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                        fontSize: 22.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w800,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    child: Text(
+                                      '@myprofile',
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ]),
                 ),
-              ]),
-            ]),*/
-
-          body: new Stack(
-            children: <Widget>[
-            Positioned(
-                width: 150.0,
-                top: MediaQuery.of(context).size.height / 20,
-                child: Column(children: <Widget>[
-                  Container(
-                      width: 100.0,
-                      height: 100.0,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/avatar.png'),
-                              fit: BoxFit.cover),
-                          borderRadius: BorderRadius.all(Radius.circular(55.0)),
-                          boxShadow: [
-                            BoxShadow(blurRadius: 7.0, color: Colors.black)
-                          ])),
-                ]))
-          ],
-        ),
-    //),
-    ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          color: Colors.transparent,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 30.0),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  subheading('My Tasks'),
+                                  /*GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => CalendarPage()),
+                                      );
+                                    },
+                                    child: calendarIcon(),
+                                  ),*/
+                                ],
+                              ),
+                              SizedBox(height: 5.0),
+                              Row(
+                                children: <Widget>[
+                                  ActiveProjectsCard(
+                                    cardColor: LightColors.kPurple,
+                                    loadingPercent: 0.25,
+                                    title: 'Mathematics',
+                                    subtitle: '8 Chapters',
+                                  ),
+                                  SizedBox(width: 20.0),
+                                  ActiveProjectsCard(
+                                    cardColor: LightColors.kRed,
+                                    loadingPercent: 0.6,
+                                    title: 'Information Security',
+                                    subtitle: '10 Chapters',
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  ActiveProjectsCard(
+                                    cardColor: LightColors.kDarkYellow,
+                                    loadingPercent: 0.45,
+                                    title: 'Digital Electronics',
+                                    subtitle: '4 Chapters',
+                                  ),
+                                  SizedBox(width: 20.0),
+                                  ActiveProjectsCard(
+                                    cardColor: LightColors.kBlue,
+                                    loadingPercent: 0.9,
+                                    title: 'Online Flutter Course',
+                                    subtitle: '9 Chapters',
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
---
+++ lib/theme/colors/light_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LightColors  {
+  static const Color kLightYellow = Color(0xFFFFF9EC);
+  static const Color kLightYellow2 = Color(0xFFFFE4C7);
+  static const Color kDarkYellow = Color(0xFFF9BE7C);
+  static const Color kPalePink = Color(0xFFFED4D6);
+
+  static const Color kRed = Color(0xFFE46472);
+  static const Color kLavender = Color(0xFFD5E4FE);
+  static const Color kBlue = Color(0xFF6488E4);
+  static const Color kLightGreen = Color(0xFFD9E6DC);
+  static const Color kGreen = Color(0xFF5E7A6C);
+  static const Color kPurple = Color(0xFFA689E0);
+  static const Color kLightPurple = Color(0xFFC997D1);
+
+  static const Color kDarkBlue = Color(0xFF0D253F);
+}
