856b4ed6d385eb2e8333f3474432e49311ea4bb3
Petrina98 <70738691+Petrina98@users.noreply.github.com>
2020-09-30 17:06:34 +0800

    Update friends_page.dart

--- lib/screens/friends_page.dart
+++ lib/screens/friends_page.dart
@@ -1,21 +1,76 @@
 import'package:flutter/material.dart';
 
-class friends_page extends StatefulWidget {
-  @override
-  _friends_pageState createState() => _friends_pageState();
-}
+class FriendDetailBody extends StatelessWidget {
+  FriendDetailBody(this.friend);
 
-class _friends_pageState extends State<friends_page> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-            title: new Text("Friends Page")
+  final Friend friend;
+
+  _createCircleBadge(IconData iconData, Color color) {
+  Widget _buildLocationInfo(TextTheme textTheme) {
+    return new Row(
+      children: <Widget>[
+        new Icon(
+          Icons.place,
+          color: Colors.white,
+          size: 16.0,
         ),
-        body: new Center(
-            child: new Text("This is friends page")
-        )
+        new Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: new Text(
+            friend.location,
+            style: textTheme.subhead.copyWith(color: Colors.white),
+          ),
+        ),
+      ],
     );
   }
-}
 
+  Widget _createCircleBadge(IconData iconData, Color color) {
+    return new Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: new CircleAvatar(
+@@ -26,33 +44,16 @@ class FriendDetailBody extends StatelessWidget {
+    var theme = Theme.of(context);
+    var textTheme = theme.textTheme;
+
+    var locationInfo = new Row(
+      children: [
+        new Icon(
+          Icons.place,
+          color: Colors.white,
+          size: 16.0,
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: new Text(
+            friend.location,
+            style: textTheme.subhead.copyWith(color: Colors.white),
+          ),
+        ),
+      ],
+    );
+
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+      children: <Widget>[
+        new Text(
+          friend.name,
+          style: textTheme.headline.copyWith(color: Colors.white),
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(top: 4.0),
+          child: locationInfo,
+          child: _buildLocationInfo(textTheme),
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+@@ -67,7 +68,7 @@ class FriendDetailBody extends StatelessWidget {
+        new Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: new Row(
+            children: [
+            children: <Widget>[
+              _createCircleBadge(Icons.beach_access, theme.accentColor),
+              _createCircleBadge(Icons.cloud, Colors.white12),
+              _createCircleBadge(Icons.shop, Colors.white12),
