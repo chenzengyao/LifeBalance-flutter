a9f9f2996e91858f6349786641fc355244173e55
Xuan Ying <leow0128@ntu.edu.sg>
2020-10-02 04:31:51 +0800

    Added a redirection to friends list page from friends_page. UI for friends page that retrieves and generates list of friend's user data from the internet To be connected using firebase Will not interfere with current edits in friend list ()
    Signed-off-by: llxyzl <leow0128@ntu.edu.sg>

---
+++ lib/screens/friends/frienddetails/friend_details_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'package:lifebalance/screens/friends/frienddetails/footer/friend_detail_footer.dart';
+import 'package:lifebalance/screens/friends/frienddetails/header/friend_detail_header.dart';
+import 'package:lifebalance/screens/friends/friends/friend.dart';
+import 'package:meta/meta.dart';
+
+import 'friend_detail_body.dart';
+
+class FriendDetailsPage extends StatefulWidget {
+  FriendDetailsPage(
+    this.friend, {
+    @required this.avatarTag,
+  });
+
+  final Friend friend;
+  final Object avatarTag;
+
+  @override
+  _FriendDetailsPageState createState() => new _FriendDetailsPageState();
+}
+
+class _FriendDetailsPageState extends State<FriendDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    var linearGradient = const BoxDecoration(
+      gradient: const LinearGradient(
+        begin: FractionalOffset.centerRight,
+        end: FractionalOffset.bottomLeft,
+        colors: <Color>[
+          const Color(0xFF413070),
+          const Color(0xFF2B264A),
+        ],
+      ),
+    );
+
+    return new Scaffold(
+      body: new SingleChildScrollView(
+        child: new Container(
+          decoration: linearGradient,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new FriendDetailHeader(
+                widget.friend,
+                avatarTag: widget.avatarTag,
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: new FriendDetailBody(widget.friend),
+              ),
+              new FriendShowcase(widget.friend),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/friends_page.dart
+++ lib/screens/friends_page.dart
@@ -1,4 +1,5 @@
 import'package:flutter/material.dart';
+import 'package:lifebalance/screens/friends/friends/friends_list_page.dart';
 
 class friends_page extends StatefulWidget {
   @override
@@ -10,7 +11,23 @@ class _friends_pageState extends State<friends_page> {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-            title: new Text("Friends Page")
+            title: new Text("Friends Page"),
+          actions: <Widget>[
+           //TEMPORARY REDIRECTION
+            // Goes to another page for friend list in case any edits are made
+            FlatButton.icon(
+              icon: Icon(Icons.people, color: Colors.white,),
+              label: Text('Friend List', style: TextStyle(color: Colors.white),),
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FriendsListPage(),
+                  ),
+                );
+              },
+            )
+          ],
         ),
         body: new Center(
             child: new Text("This is friends page")
